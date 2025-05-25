cc4ad3ed88568c7d23f813b8655c5f6721896b06
PirelaJuan <pirelajuan13@gmail.com>
2025-05-25 18:49:49 -0400

    Modify multiple Files to do better practices of modules

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,24 @@
 export const cart = [];
 
 
+export function addToCart(productId){
+  let machtchingItem;
+    cart.forEach((cartItem)=> {
+
+      if(productId === cartItem.productId){
+        machtchingItem = cartItem;
+      }
+    });
+
+    if (machtchingItem){
+      machtchingItem.quantity +=1
+    }else{
+       cart.push({
+        productId: productId,
+        quantity: 1
+      });
+    }
+}
+
+
 
